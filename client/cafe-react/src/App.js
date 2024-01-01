d9b9997e0ccd2068c83121621c48770784034c3c
Oleksandr Proniuk <aleksandrpronyk@gmail.com>
2024-01-01 21:50:40 +0200

    First Change layout on the FE

--- client/cafe-react/src/App.js
+++ client/cafe-react/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Container, Grid } from "@mui/material";
+import Header from "./components/Header";
+import Dish from "./components/Dish";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Container sx={{ mt: "10px", mb: "10px" }}>
+        <Grid container spacing={3}>
+          <Dish />
+          <Dish />
+          <Dish />
+          <Dish />
+          <Dish />
+          <Dish />
+        </Grid>
+      </Container>
+    </>
   );
 }
 
